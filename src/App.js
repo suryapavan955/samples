f7b31988de60bfedebfc881b4af3fce2eecffe9e
suryapavan955 <suryapavan689@gmail.com>
2025-07-01 14:35:48 +0530

    added navbar file and changes in app.css and app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const array =[
+  {id:1 , Name:"Polo"},
+  {id:2 , Name:"Puma"},
+  {id:3 , Name:"PeterEngland"},
+  {id:4 , Name:"Teamspirit"},
+  {id:5 , Name:"Nike"},
+]
+
 
 function App() {
+
+  const [list,setList] =useState('');
+ 
+ 
+
+const filtered= list ? array.filter(item=> 
+  item.Name.toLocaleLowerCase().includes(list.toLocaleLowerCase())
+) : array;
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
+    <>
+      <input value={list} onChange={ (e) => setList(e.target.value) } placeholder='Enter the value' />
+      <button >Add me to cart</button>
+
+      {/* {
+        array.map ((item)=>(
+          <p key ={item.id}>{item.Name}</p>
+        ))
+      } */}
+
+      <div>{
+        filtered.map((item) => (
+        <p key ={item.id}>{item.Name}</p>
+        ))
+      }
+ 
+      </div>
+
+    </>
   );
 }
 
